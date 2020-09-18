ca9edae4268099e6b233bf0b316516a0980827d5
Julie Ann Delda <70535615+JDftw@users.noreply.github.com>
2020-09-18 13:40:51 +0800

    Add files via upload

---
+++ grab_on_spyder.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 16 23:39:29 2020
+
+@author: Julie Ann Delda
+"""
+
+import pandas as pd
+
+df = pd.read_csv('DataSeerGrabPrizeData.csv')
+
+df.describe()
+
+df.dropna()
+
+df.describe()
+
+df.to_csv('grab_on_spyder.csv', index=False)
